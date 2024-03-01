c810bd4035b1ac9ae14dcd09ccca7b97ca081cde
nipunikwickramasooriya <hrnkwickramasooriya@gmail.com>
2024-03-01 12:00:22 +0530

    modified

--- leader/includes/left_sidebar.php
+++ leader/includes/left_sidebar.php
@@ -15,26 +15,6 @@
 						
 					</li>
 
-					<li class="dropdown">
-						<a href="apply_leave.php" class="dropdown-toggle no-arrow">
-							<span class="micon dw dw-house-1"></span><span class="mtext">Apply Leave</span>
-						</a>
-						
-					</li>
-					
-				
-					<li class="dropdown">
-						<a href="javascript:;" class="dropdown-toggle">
-							<span class="micon dw dw-apartment"></span><span class="mtext"> Leave </span>
-						</a>
-						<ul class="submenu">
-							<li><a href="leaves.php">All Leave</a></li>
-							<li><a href="pending_leave.php">Pending Leave</a></li>
-							<li><a href="approved_leave.php">Approved Leave</a></li>
-							<li><a href="rejected_leave.php">Rejected Leave</a></li>
-						</ul>
-					</li> 
-
 					<li>
 						<div class="dropdown-divider"></div>
 					</li>
